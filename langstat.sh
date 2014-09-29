930a81980c2df578ff77595ea668db84a6f22942
Christophe MARTY <mcfly34@gmail.com>
2014-09-30 01:14:47 +0200

    Modification de l'option -help + Ajout de commentaire (Français / Anglais)

--- langstat.sh
+++ langstat.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#################################################################################################
+#												#
+# type the commande : ./langstat.sh dico.txt -help  (for use help of this script)		#
+# tappez la commande : ./langstat.sh dico.txt -help (pour utiliser l'aide de se script)		#
+#												#
+#################################################################################################
+
 # Vide la console
 clear
 
@@ -41,16 +48,19 @@ else
 				;;
 				# Retourne dans combien de mot la lettre passée en paramètre exite
 				"-a" | "-letter_alpha")
+					# vérifie que le 3eme paramètre existe
 					if [ -z $3 ]; then
 						clear
 						echo "Pour l'option [$2] il manque en 3ème paramètre, une lettre !"
 					else
 						clear
+						# Vérifie que le 3eme paramètre ne contient qu'une seule lettre
 						cmd=`echo $3 | wc -c`
 						let "test = $cmd - 1"
 						if [ $test -gt 1 ]; then
 							echo "Il faut saisir une seule lettre avec l'option [$2], actuellement : $test !"
 						else
+							# Retourne le résultat pour la lettre
 							l=`echo $3 | tr '[:lower:]' '[:upper:]'`
 							echo "Dans combien de mot la lettre [$l] existe :"
 							grep -i $3 $temp
@@ -78,12 +88,17 @@ else
 						echo -e "SYNOPSIS : \n\t./langstat.sh dico.txt [OPTION]"
 						echo -e "\t./langstat.sh dico.txt [-letter_alpha A-Z]\n"
 						echo -e "DESCRIPTION : \n\tliste des options :\n"
-						echo -e "\t\t-r, -reverse : \t\tinverse le résultat, de la lettre la moins utilisée à la plus utilisée\n"
-						echo -e "\t\t-a, -letter_alpha : \tpermet de passer la lettre pour lequel on veut savoir le nombre de mot ou elle est utilisée\n"
-						echo -e "\t\t-l, -list : \t\tpermet de voir la liste de mot\n"
-						echo -e "\t\t-c, -count : \t\tnombre de mot de 'dico.txt\n"
-						echo -e "\t\t-h, -help : \t\taide de la commande\n"
-						read -p 'Tapez q pour sortir : ' -n 1 sortie
+						echo -e "\t\t-r, -reverse : \t\tinverse le résultat, de la lettre la moins utilisée à la plus utilisée"
+						echo -e "\t\t\t\t\t(reverse result)\n"
+						echo -e "\t\t-a, -letter_alpha : \tpermet de passer la lettre pour lequel on veut savoir le nombre de mot ou elle est utilisée"
+						echo -e "\t\t\t\t\t(choose letter for result)\n"
+						echo -e "\t\t-l, -list : \t\tpermet de voir la liste de mot"
+						echo -e "\t\t\t\t\t(list word in 'dico.txt')\n"
+						echo -e "\t\t-c, -count : \t\tnombre de mot de 'dico.txt'"
+						echo -e "\t\t\t\t\t(word count in 'dico.txt')\n"
+						echo -e "\t\t-h, -help : \t\taide de la commande"
+						echo -e "\t\t\t\t\t(command help)\n"
+						read -p 'Tapez [q] pour sortir : ' -n 1 sortie
 					done
 					clear
 				;;
